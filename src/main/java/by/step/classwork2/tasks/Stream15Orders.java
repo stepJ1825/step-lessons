2cba9aca714e0d979e6f53765000f92c681001e8
ivanvikvik <ivanvikvik@gmail.com>
2025-09-01 19:21:00 +0300

    Solved some tasks

--- src/main/java/by/step/classwork2/tasks/Stream15Orders.java
+++ src/main/java/by/step/classwork2/tasks/Stream15Orders.java
@@ -4,19 +4,46 @@ import by.step.classwork2.model.Order15;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 public class Stream15Orders {
     public static void main(String[] args) {
         List<Order15> orders = Arrays.asList(
-                new Order15("2023-01", "CustomerA", 100),
-                new Order15("2023-01", "CustomerB", 200),
-                new Order15("2023-02", "CustomerA", 150),
-                new Order15("2023-02", "CustomerC", 300)
+                new Order15("2023-JAN", "CustomerA", 100),
+                new Order15("2023-JAN", "CustomerB", 200),
+                new Order15("2023-FEB", "CustomerA", 150),
+                new Order15("2023-APR", "CustomerA", 150),
+                new Order15("2023-MAR", "CustomerA", 150),
+                new Order15("2023-MAR", "CustomerA", 250),
+                new Order15("2023-FEB", "CustomerC", 300)
         );
 
         // 1. Сгруппируйте заказы по месяцам в TreeMap
+        Map<String, List<Order15>> collect = orders.stream()
+                .collect(Collectors.groupingBy(order ->
+                                order.getOrderDate().split("-")[1],
+                        TreeMap::new,  // Отсортированная Map по ключам
+                        Collectors.toList()));
+
         // 2. Найдите общую сумму заказов для каждого месяца
+        Map<String, Integer> collect2 = orders.stream()
+                .collect(Collectors.groupingBy(order ->
+                                order.getOrderDate().split("-")[1],
+                                Collectors.summingInt(Order15::getCost)));
+
         // 3. Для каждого месяца получите список уникальных клиентов
+        Map<String, List<String>> collectedDistinctCustomers = orders.stream()
+                .collect(Collectors.groupingBy(order ->
+                                order.getOrderDate().split("-")[1],
+                        Collectors.collectingAndThen(
+                                Collectors.toList(),
+                                list -> list.stream()
+                                        .map(Order15::getCustomer)
+                                        .distinct()
+                                        .toList())
+                ));
 
         // Для каждого месяца найдите:
         // 4. Общую сумму заказов в формате "$XХХХ.XX"
