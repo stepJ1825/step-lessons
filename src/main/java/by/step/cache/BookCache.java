a880c34e821bbde934aa2bf1ced53bcf5c4daa83
ivanvikvik <ivanvikvik@gmail.com>
2025-09-24 21:33:50 +0300

    Modified cache with WeakReference

--- src/main/java/by/step/cache/BookCache.java
+++ src/main/java/by/step/cache/BookCache.java
@@ -2,25 +2,27 @@ package by.step.cache;
 
 import by.step.model.Book;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
+import java.util.*;
 
 public class BookCache {
     private Date creationDate = new Date();
-    private final List<Book> library = new ArrayList<>();
+    //    private final List<Book> library = new ArrayList<>();
+    private final List<WeakReference<Book>> library = new ArrayList<>();
 
     public List<Book> getAllBooks() {
         Date readAllBooksDate = new Date();
         long timeFromStart = readAllBooksDate.getTime() - creationDate.getTime();
         if (timeFromStart < 6000L) {
-            return new ArrayList<>(library);
+            return library.stream()
+                    .map(Reference::get)
+                    .toList();
         } else return new ArrayList<>();
     }
 
     public void fillCache(List<Book> books) {
-        library.addAll(books);
+        books.forEach(book -> library.add(new WeakReference<>(book)));
         creationDate = new Date();
         //например  21:22:22 - всё ОК.
         //в 21:23:22 - кэш невалидный, надо чистить
