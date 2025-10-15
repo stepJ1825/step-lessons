616353459727bd7f0bc7b763989ab14878105a3a
alexei <alexei@alexei.by>
2025-10-15 19:18:54 +0300

    Added exam results.

--- src/main/java/by/step/BoxInitialization.java
+++ src/main/java/by/step/BoxInitialization.java
@@ -2,12 +2,25 @@ package by.step;
 
 public class BoxInitialization {
     public static void main(String[] args) {
-        Box<? super String> b1 = new Box<> (123);
+        Box<? super String> b1 = new Box<> (new Snail()); //123 компилятор видит как Object
+//        String value = (String) b1.value;
+//        Object value = b1.value;
+//        System.out.println(value.getClass());
+//        System.out.println(b1.getClass());
+
         Box<? extends String> b2 = new Box<>("123");
-        Box<? extends Number> b3 = new Box<>(123);
+//        String value1 = b2.value;
+//        Class<? extends String> aClass = value1.getClass();
+//        if (aClass.equals(String.class)) {
+//            System.out.println("String class");
+//        }
+
+        Box<? extends Number> b3 = new Box<>(123D);
+//        Integer b3IntegerValue = (Integer) b3.value;
+//        Double b3DoubleValue = (Double) b3.value;
+
         Box<? super Number> b4 = new Box<>(123L);
-        System.out.println(b1.value.getClass());
-        System.out.println(b1.getClass());
+//        Object value = b4.value;
     }
 }
 
