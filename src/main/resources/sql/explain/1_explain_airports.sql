4f6a83a2e42d3eb05dea57f0dc405322e5487771
alexei <alexei@alexei.by>
2025-11-17 20:09:29 +0300

    Added explain examples from lesson

--- src/main/resources/sql/explain/1_explain_airports.sql
+++ src/main/resources/sql/explain/1_explain_airports.sql
@@ -1,4 +1,4 @@
---29
+--29.   1.8..2.0 sec
 explain (ANALYZE)
 select t.passenger_name, count(f.flight_id) from tickets t
 join boarding_passes bp using (ticket_no)
@@ -7,12 +7,36 @@ where f.scheduled_departure >= CURRENT_DATE - interval '3 year'
 	and f.scheduled_departure <= CURRENT_DATE
 group by t.passenger_name
 having count(f.flight_id) > 5
-order by count(f.flight_id) DESC
+order by count(f.flight_id) desc
 
 
+
+
+
+explain (ANALYZE)
+select * from flights f
+where f.scheduled_departure >= CURRENT_DATE - interval '1 month'
+	and f.scheduled_departure <= CURRENT_DATE
+union all
+select * from flights f
+where f.scheduled_departure >= CURRENT_DATE - interval '32 day'
+	and f.scheduled_departure <= CURRENT_DATE - interval '1 month'
+
+
+explain (ANALYZE)
+select f.scheduled_departure from flights f
+where f.scheduled_departure >= CURRENT_DATE - interval '1 month'
+	and f.scheduled_departure <= CURRENT_DATE
+
+explain (ANALYZE)
+select f.* from flights f
+where f.route_no in
+(select r.route_no from routes r where r.airplane_code = '339')
 
 --22. Для каждого маршрута (например, LHR → JFK) присвойте ранг
 --каждому рейсу по стоимости самого дорогого билета.
+-- 486ms без доп индекса
+explain (ANALYZE)
 select  b.book_ref,
 		r.route_no,
 		b.total_amount,
@@ -28,3 +52,6 @@ join routes r using (route_no)
 where r.departure_airport = 'LHR'
 		and r.arrival_airport = 'ORY'
 order by "rank"
+
+create index routes_dep_arr_aitports
+on routes (departure_airport, arrival_airport)
---
+++ src/main/resources/sql/explain/README.md
@@ -0,0 +1,4 @@
+#### Что почитать:
+- [официальная документация](https://postgrespro.ru/docs/postgresql/17/using-explain)
+- [неплохая небольшая статья](https://serverspace.ru/support/glossary/explain-analyze/?utm_source=google.com&utm_medium=organic&utm_campaign=google.com&utm_referrer=google.com)
+- [продвинутый анализ c pgstatstatements](https://postgrespro.ru/docs/postgresql/17/pgstatstatements)
