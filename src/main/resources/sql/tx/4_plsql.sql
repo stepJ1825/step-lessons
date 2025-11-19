d7960663acf05875354ad9ae1c567a4ec8d85a6b
Шумилов Алексей Александрович <Shumilov@alusoft.by>
2025-11-19 17:07:55 +0300

    Added new function

--- src/main/resources/sql/tx/4_plsql.sql
+++ src/main/resources/sql/tx/4_plsql.sql
@@ -8,7 +8,7 @@ BEGIN
     -- Проверка, что у отправителя достаточно средств
     IF (SELECT balance FROM accounts WHERE id = from_acc_id FOR UPDATE)
 			< amount
-    THEN RAISE EXCEPTION 'Insufficient funds for account %', from_acc_id; --TODO: решить проблему с зависанием
+    THEN RAISE EXCEPTION 'Insufficient funds for account %', from_acc_id;
     END IF;
 
     -- Выполняем перевод
@@ -38,3 +38,21 @@ select * from accounts a
 --Откат до sp1 — перевод 2 отменён.
 --Перевод 3: Alice -> Bob (30 рублей) — успешно.
 --Транзакция зафиксирована.
+
+
+--------------------------------------------------------------------------------------
+-- DROP FUNCTION public.try_transfer_with_savepoint(int4, int4, numeric);
+
+CREATE OR REPLACE FUNCTION public.try_transfer_with_savepoint(from_acc_id integer, to_acc_id integer, amount numeric)
+ RETURNS text
+ LANGUAGE plpgsql
+AS $function$
+BEGIN
+    PERFORM transfer_money(from_acc_id, to_acc_id, amount);
+    RETURN 'OK';
+EXCEPTION
+    WHEN OTHERS THEN
+        RETURN 'ERROR: ' || SQLERRM;
+END;
+$function$
+;
